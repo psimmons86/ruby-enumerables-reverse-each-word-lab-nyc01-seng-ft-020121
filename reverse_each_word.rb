7ddfde09c88a7bb193fdfd1ba15a89fcf0cc6658
patrick simmons <hadroncollides@gmail.com>
2021-01-26 20:07:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,6 @@
 
 
-def reverse_each_word("Hello there, and how are you?").reverse.each
+def reverse_each_word("Hello there, and how are you?")
+  "Hello there, and how are you?".reverse.each
+end
+end
