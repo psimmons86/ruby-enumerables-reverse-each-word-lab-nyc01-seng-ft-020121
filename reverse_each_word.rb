b03340f5c1882c3fed5953c7c37315636b8d16f6
patrick simmons <hadroncollides@gmail.com>
2021-01-26 20:10:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,2 @@
 
-def reverse_each_word("Hello there, and how are you?")
-  reversed = "Hello there, and how are you?".split(" ").collect do |word|
-  word.reverse
-end
+"Hello there, and how are you?"
