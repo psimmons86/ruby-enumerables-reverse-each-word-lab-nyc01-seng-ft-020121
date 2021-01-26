36bc5684d47e12974bbcab340181bfdfacd4fbb8
patrick simmons <hadroncollides@gmail.com>
2021-01-26 20:12:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 phrase = "Hello there, and how are you?"
 
 def reverse_each_word(phrase)
-  reversed = sentence.split(" ").collect do |word|
+  reversed = phrase.split(" ").collect do |word|
   word.reverse
 end
  reversed.join(" ")
